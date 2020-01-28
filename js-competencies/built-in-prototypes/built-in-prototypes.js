3924348033f8ad7477d28c545c44ddf45c396b8c
Joshua Smith <joshuasmith@Joshs-MacBook-Pro.local>
2020-01-28 09:49:32 -0700

    completed built-in-prototypes section

--- js-competencies/built-in-prototypes/built-in-prototypes.js
+++ js-competencies/built-in-prototypes/built-in-prototypes.js
@@ -1,6 +1,5 @@
 //+++++++++ Start Here! All problems are below. +++++++++//
 
-
 //////////////////PROBLEM 1////////////////////
 
 //Write a function called dragonMapper that takes in an 
@@ -10,8 +9,12 @@
 //use the built-in array method .map
   
   //Code here
-
-
+function dragonMapper(array) {
+  let newArray = array.map(element => {
+    return `${element}...here be dragons`;
+  })
+  return newArray;
+}
 
 //////////////////PROBLEM 2////////////////////
 
@@ -21,7 +24,16 @@
 //cookieLoversOnly should return the filtered array.
   
   //Code here
-
+function cookieLoversOnly(array) {
+  let newArr = array.filter(element => {
+    for (let key in element) {
+      if (key === "favoriteCookie") {
+        return element;
+      }
+    }
+  })
+  return newArr;
+}
 
 //////////////////PROBLEM 3////////////////////
 
@@ -33,10 +45,13 @@
 //the callback function as an argument every time it is invoked.
 //Hint: there is an array method for this.
 
-
   //Code here
-
-
+function changeValue(array, callback) {
+  let newArray = array.forEach(element => {
+    callback(element);
+  });
+  return newArray;
+}
 
 //////////////////PROBLEM 4////////////////////
 
@@ -47,7 +62,12 @@
 //Return the product.
   
   //Code here
-
+function findProduct(array) {
+  let newArray = array.reduce((acc, element) => {
+    return acc * element;
+  })
+  return newArray;
+}
 
 //////////////////PROBLEM 5////////////////////
 
@@ -64,8 +84,10 @@
 // cipherize will return: "d"
 
   //Code here
-
-
+function cipherize(array, string) {
+  let index = array.indexOf(string);
+  return string.charAt(index);
+}
 
 //////////////////PROBLEM 6////////////////////
 
@@ -75,3 +97,8 @@
 //apart, reversing it, and putting it back together again.
 
   //Code here
+function reverseMe(string) {
+  let stringArray = string.split("");
+  let reversed = stringArray.reverse();
+  return reversed.join("");
+}
